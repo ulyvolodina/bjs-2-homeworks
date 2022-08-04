5a036ba1107aea0df8a8c8760772b76838cdf4d9
Ulya <ulyvolodina@gmail.com>
2022-08-04 09:51:15 +0300

    discriminant

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,6 +1,22 @@
+"use strict";
+
 function solveEquation(a, b, c) {
-  let arr;
+  let arr = [];
   // код для задачи №1 писать здесь
+  let D = b**2 - 4*a*c;
+
+  if (D === 0) {
+	arr[0] = -b/(2*a);
+  } else if (D > 0) {
+
+
+	arr[0] = (-b + Math.sqrt(D)) / (2*a);
+	arr[1] = (-b - Math.sqrt(D)) / (2*a);
+  } else {
+	arr = ['', ''];
+  }
+
+
   return arr; // array
 }
 
