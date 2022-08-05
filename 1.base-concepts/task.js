ebbff1e367ec8278526342fbc656b9fa5806b10f
Ulya <ulyvolodina@gmail.com>
2022-08-05 08:28:02 +0300

    corrected

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -3,17 +3,13 @@
 function solveEquation(a, b, c) {
   let arr = [];
   // код для задачи №1 писать здесь
-  let D = b**2 - 4*a*c;
+  let discriminant = b**2 - 4*a*c;
 
-  if (D === 0) {
+  if (discriminant === 0) {
 	arr[0] = -b/(2*a);
-  } else if (D > 0) {
-
-
-	arr[0] = (-b + Math.sqrt(D)) / (2*a);
-	arr[1] = (-b - Math.sqrt(D)) / (2*a);
-  } else {
-	arr = ['', ''];
+  } else if (discriminant > 0) {
+	arr[0] = (-b + Math.sqrt(discriminant)) / (2*a);
+	arr[1] = (-b - Math.sqrt(discriminant)) / (2*a);
   }
 
 
