0ca97a19139eed6afcc8a39468d35c25bf4eef10
Ulya <ulyvolodina@gmail.com>
2022-08-08 15:52:53 +0300

    need fix

---
+++ 6.exception-closure/task.js
@@ -0,0 +1,69 @@
+// ## Задача №1. Форматтер чисел
+
+function parseCount(someValue) {
+	let parse = Number.parseInt(someValue);
+	if (isNaN(parse)) {
+		throw new Error("Невалидное значение");
+	} else return parsed;
+}
+
+function validateCount(someValue) {
+	try {
+		let parse = parseCount(someValue);
+		return parse;
+	} catch (error) {
+		return error;
+	}
+}
+
+// ## Задача №2. Треугольник 
+class Triangle {
+constructor(a, b, c) {
+	if (a + b < c || a + c < b || b + c < a) {
+		throw new Error("Треугольник с такими сторонами не существует");
+	} else {
+		this.a = a;
+		this.b = b;
+		this.c = c;
+	}
+}
+
+getPerimeter() {
+	let perimetr = (this.a + this.b + this.c);
+	return perimetr;
+}
+
+getArea () {
+	let perimetr = this.getPerimeter() / 2;
+	let area = Math.sqrt(perimetr * (perimetr - this.a) * (perimetr - this.b) * (perimetr - this.c));
+	this.area = area;
+
+	return this.area;
+}
+
+function getTriangle(a, b, c) {
+	try {
+		return new Triangle(a, b, c);
+	} catch (error) {
+		return {
+		getArea: () => "Ошибка! Треугольник не существует",
+		getPerimeter: () => "Ошибка! Треугольник не существует",
+		};
+	}
+}
+
+const triangle = new Triangle(6,10,15);
+// console.log(triangle.getPerimeter());//(12);
+console.log(triangle.getArea()); //(4.899);
+
+// const triangle = new Triangle(6,10,15);
+// console.log(triangle.getPerimeter());//(31);
+// console.log(triangle.getArea()); //(20123);
+        	       
+ 
+// console.log(new Triangle(1,3,100))
+// console.log(new Triangle(100,3,10))
+// console.log(new Triangle(1,300,10))
+
+
+// console.log(getTriangle(2,5,5)); 
